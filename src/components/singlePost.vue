ed04f9c81498cca3141edc56c4046d93328d342b
xina0x <sinapir2@gmail.com>
2021-06-04 17:41:44 +0430

    Change like style in home

--- src/components/singlePost.vue
+++ src/components/singlePost.vue
@@ -5,13 +5,22 @@
         <img src="../../public/img/avatar.jpg" alt="" style="width: 30px; height: 30px; border-radius: 50%; float: left;">
         <p class="userId">madman</p>
         <vs-button
-            style="display: inline-block; float: right;"
+            style="display: inline-block; float: right; margin-top: 0;"
             flat
             circle
             icon
             color="rgb(59,89,153)"
         >
-          <i class="iconify" data-icon="bx:bxs-bookmark" data-inline="false"></i><p style="color: white;"> Add to watch list</p>
+          <i class="iconify" data-icon="bx:bx-bookmark" data-inline="false"></i>
+        </vs-button>
+        <vs-button
+            style="display: inline-block; float: right; margin-top: 0;"
+            flat
+            circle
+            icon
+            color="rgb(59,89,153)"
+        >
+          <i class="iconify" data-icon="bx:bx-like" data-inline="false"></i><p style="color: white; margin-left: 5px;"> 43</p>
         </vs-button>
         <h3>Interstellar</h3>
       </template>
@@ -25,20 +34,15 @@
         <p>
           <br>
           Lorem ipsum dolor sit amet consectetur, adipisicing elit ....
+
           <vs-button
-              style="display: inline;"
+              style="display: inline; margin-top: 1rem;"
               flat
           >
             View full post
           </vs-button>
         </p>
         <small style="opacity: 0.3;"><br> 4 hours ago</small>
-      </template>
-      <template #interactions>
-        <vs-button danger icon>
-          <i class="iconify" data-icon="bx:bx-heart" data-inline="false"></i>
-          <span>45</span>
-        </vs-button>
       </template>
     </vs-card>
   </vs-col>
@@ -104,5 +108,10 @@ export default {
 }
 .home >>> .vs-card:hover img{
   transform: scale(1,1) !important;
+}
+.home >>> .vs-card__interactions{
+  left:40%;
+  bottom:-4%;
+  width: 100%;
 }
 </style>
