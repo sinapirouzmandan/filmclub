e2ef013f60ffd66d0641fb90012a3e8e6eb7e899
Xina 0x <xina0x@protonmail.com>
2021-07-07 22:54:33 +0430

    change new post header image place holder

--- src/components/NewPost/postDetail.vue
+++ src/components/NewPost/postDetail.vue
@@ -4,6 +4,15 @@
     <input ref="file" accept="image/*" class="uploadHeader" type="file" @change="handleProfileUploads">
     <div class="headerImage">
       <img id="headerSelected" src="false" v-show="file">
+      <svg fill="#5c5c5c" aria-hidden="true" class="changePostHeader"
+           viewBox="0 0 24 24">
+        <g>
+          <path
+              d="M19.708 22H4.292C3.028 22 2 20.972 2 19.708V7.375C2 6.11 3.028 5.083 4.292 5.083h2.146C7.633 3.17 9.722 2 12 2c2.277 0 4.367 1.17 5.562 3.083h2.146C20.972 5.083 22 6.11 22 7.375v12.333C22 20.972 20.972 22 19.708 22zM4.292 6.583c-.437 0-.792.355-.792.792v12.333c0 .437.355.792.792.792h15.416c.437 0 .792-.355.792-.792V7.375c0-.437-.355-.792-.792-.792h-2.45c-.317.05-.632-.095-.782-.382-.88-1.665-2.594-2.7-4.476-2.7-1.883 0-3.598 1.035-4.476 2.702-.16.302-.502.46-.833.38H4.293z"></path>
+          <path
+              d="M12 8.167c-2.68 0-4.86 2.18-4.86 4.86s2.18 4.86 4.86 4.86 4.86-2.18 4.86-4.86-2.18-4.86-4.86-4.86zm2 5.583h-1.25V15c0 .414-.336.75-.75.75s-.75-.336-.75-.75v-1.25H10c-.414 0-.75-.336-.75-.75s.336-.75.75-.75h1.25V11c0-.414.336-.75.75-.75s.75.336.75.75v1.25H14c.414 0 .75.336.75.75s-.336.75-.75.75z"></path>
+        </g>
+      </svg>
       <div class="xinu">
         <vs-button
             v-if="!file"
@@ -241,9 +250,7 @@ export default {
   max-width: 500px;
   height: 250px;
   margin: 0 auto;
-  background: url('../../assets/nullImage.jpg');
-  background-size: cover;
-  background-position: center;
+  background-color: white;
   display: flex;
   justify-content: center;
   justify-items: center;
@@ -373,5 +380,10 @@ p {
   display: inline-block;
   text-align: center;
 }
-
+.changePostHeader{
+  position: absolute;
+  width: 40px;
+  height:40px;
+  margin-top: 125px;
+}
 </style>
